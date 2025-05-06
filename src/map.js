8517f055898bd2445967bb6286884abfdaf0a594
Aradhya2005 <73036479+Aradhya2005@users.noreply.github.com>
2025-05-05 23:05:45 -0700

    Update map.js

--- src/map.js
+++ src/map.js
@@ -1,136 +1,94 @@
-// Add these constants at the top of map.js
-const TERRAIN_TYPES = {
-    GRASS: [0, 1, 2, 43],
-    ROAD: [12, 13, 14, 24, 25, 26, 36, 37, 38, 39, 40, 41, 42],
-    WATER: [3, 4, 5, 15, 16, 17, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-};
+class Map extends Phaser.Scene {
+    constructor() {
+        super("Map");
+    }
 
-// Add these new methods to the Map class
-generateBiomes(width, height) ;
-    // Create a heightmap for biome distribution
-    const heightmap = this.createHeightmap(width, height);
-    
-    // Determine biome for each cell
-    const biomeMap = [];
-    for (let y = 0; y < height; y++) {
-        biomeMap[y] = [];
-        for (let x = 0; x < width; x++) {
-            const value = heightmap[y][x];
-            
-            if (value < 0.3) {
-                biomeMap[y][x] = 'WATER';
-            } else if (value < 0.7) {
-                biomeMap[y][x] = 'GRASS';
-            } else {
-                biomeMap[y][x] = 'ROAD';
+    create() {
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+        map = this.make.tilemap({ tileWidth: 16, tileHeight: 16, width: 20, height: 15 });
+        tileset = map.addTilesetImage('tiles', null, 16, 16, 0, 0);
+        ground = map.createBlankLayer("WFC Layer", tileset);
+        decoration = map.createBlankLayer("Decoration Layer", tileset);
+
+        waveTable = [];
+        defaultTable = [0, 1, 2, 12, 13, 14, 24, 25, 26, 36, 37, 38, 39, 40, 41, 42, 43];
+
+        this.initializeWaveTable(waveTable, defaultTable, map.width, map.height);
+
+        tileData = {
+            0: { name: "plainGrass", weight: 20, edges: { top: "GGG", right: "GGG", bottom: "GGG", left: "GGG" }},
+            1: { name: "detailGrass", weight: 15, edges: { top: "GGG", right: "GGG", bottom: "GGG", left: "GGG" }},
+            2: { name: "flowerGrass", weight: 10, edges: { top: "GGG", right: "GGG", bottom: "GGG", left: "GGG" }},
+            12: { name: "leftUCornerRoad", weight: 1, edges: { top: "GGG", right: "GDD", bottom: "GDD", left: "GGG" }},
+            13: { name: "topRoad", weight: 1, edges: { top: "GGG", right: "GDD", bottom: "DDD", left: "GDD" }},
+            14: { name: "rightUCornerRoad", weight: 1, edges: { top: "GGG", right: "GGG", bottom: "DDG", left: "GDD" }},
+            24: { name: "leftRoad", weight: 1, edges: { top: "GDD", right: "DDD", bottom: "GDD", left: "GGG" }},
+            25: { name: "centerRoad", weight: 1, edges: { top: "DDD", right: "DDD", bottom: "DDD", left: "DDD" }},
+            26: { name: "rightRoad", weight: 1, edges: { top: "DDG", right: "GGG", bottom: "DDG", left: "DDD" }},
+            36: { name: "leftBCornerRoad", weight: 1, edges: { top: "GDD", right: "DDG", bottom: "GGG", left: "GGG" }},
+            37: { name: "bottomRoad", weight: 1, edges: { top: "DDD", right: "DDG", bottom: "GGG", left: "DDG" }},
+            38: { name: "rightBCornerRoad", weight: 1, edges: { top: "DDG", right: "GGG", bottom: "GGG", left: "DDG" }},
+            39: { name: "allRoad1", weight: 1, edges: { top: "DDD", right: "DDD", bottom: "DDD", left: "DDD" }},
+            40: { name: "allRoad2", weight: 1, edges: { top: "DDD", right: "DDD", bottom: "DDD", left: "DDD" }},
+            41: { name: "allRoad3", weight: 1, edges: { top: "DDD", right: "DDD", bottom: "DDD", left: "DDD" }},
+            42: { name: "allRoad4", weight: 1, edges: { top: "DDD", right: "DDD", bottom: "DDD", left: "DDD" }},
+            43: { name: "grassStone", weight: 2, edges: { top: "GGG", right: "GGG", bottom: "GGG", left: "GGG" }},
+        };
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyS)) {
+            if (!started) {
+                this.WFC(waveTable, tileData, map_width, map_height, ground);
+                // this.WFCLexical(waveTable, tileData, map_width, map_height, ground);
+                this.placeDecorations(map_width, map_height, decoration);
+                started = true;
             }
         }
     }
-    
-    return biomeMap;
-}
 
-createHeightmap(width, height) {
-    // Simple noise-based heightmap
-    const heightmap = [];
-    const scale = 0.1;
-    
-    for (let y = 0; y < height; y++) {
-        heightmap[y] = [];
-        for (let x = 0; x < width; x++) {
-            // Simple noise function - replace with Perlin noise for better results
-            const value = Math.abs(Math.sin(x * scale) * Math.cos(y * scale));
-            heightmap[y][x] = (value + 1) / 2; // Normalize to 0-1
+    defineBiomes(width, height) {
+        const biomeMap = [];
+        for (let y = 0; y < height; y++) {
+            biomeMap[y] = [];
+            for (let x = 0; x < width; x++) {
+                if (x < width / 2) {
+                    biomeMap[y][x] = "grass";
+                } else if (y < height / 2) {
+                    biomeMap[y][x] = "road";
+                } else {
+                    biomeMap[y][x] = "stone";
+                }
+            }
         }
+        return biomeMap;
     }
-    
-    return heightmap;
-}
 
-initializeBiomeSeeds(waveTable, biomeMap, tileData) {
-    // Place seed tiles for each biome
-    for (let y = 0; y < biomeMap.length; y++) {
-        for (let x = 0; x < biomeMap[0].length; x++) {
-            const biome = biomeMap[y][x];
-            const possibleTiles = TERRAIN_TYPES[biome];
-            
-            // Only initialize if this cell hasn't been processed yet
-            if (!waveTable[y][x].collapsed) {
-                waveTable[y][x].possibilities = possibleTiles.slice();
-                waveTable[y][x].entropy = possibleTiles.length;
+    initializeWaveTable(waveTable, defaultTable, width, height) {
+        const biomeMap = this.defineBiomes(width, height);
+        for (let y = 0; y < height; y++) {
+            waveTable[y] = [];
+            for (let x = 0; x < width; x++) {
+                let allowed;
+                const biome = biomeMap[y][x];
+
+                if (biome === "grass") {
+                    allowed = [0, 1, 2, 43];  // grassy tiles
+                } else if (biome === "road") {
+                    allowed = [12, 13, 14, 24, 25, 26, 36, 37, 38, 39, 40, 41, 42];  // road tiles
+                } else if (biome === "stone") {
+                    allowed = [43, 0];  // mostly stone, some grass
+                } else {
+                    allowed = defaultTable;
+                }
+
+                waveTable[y][x] = {
+                    collapsed: false,
+                    options: allowed.slice()
+                };
             }
         }
     }
-}
-
-// Modify your WFC method to use biomes:
-WFC(waveTable, tileData, width, height, layer) {
-    // First generate biome map
-    const biomeMap = this.generateBiomes(width, height);
-    
-    // Initialize wave table with biome constraints
-    this.initializeBiomeSeeds(waveTable, biomeMap, tileData);
-    
-    // Place initial seed tiles
-    const startX = Math.floor(Math.random() * width);
-    const startY = Math.floor(Math.random() * height);
-    const startBiome = biomeMap[startY][startX];
-    const startTileIndex = TERRAIN_TYPES[startBiome][
-        Math.floor(Math.random() * TERRAIN_TYPES[startBiome].length)
-    ];
-    
-    const startCell = waveTable[startY][startX];
-    startCell.possibilities = [startTileIndex];
-    startCell.entropy = 0;
-    startCell.collapsed = true;
-    layer.putTileAt(startTileIndex, startX, startY);
-    this.propagate(waveTable, tileData, startCell, layer);
-
-    // Rest of the WFC algorithm remains the same...
-    let current = startCell;
-    let currentTries = 1;
-    const maxTries = width * height;
-
-    while (currentTries <= maxTries) {
-        let nextCell = this.findLowestEntropyCell(waveTable);
-        if (!nextCell) break;
-
-        const tileIndex = this.weightedPick(nextCell.possibilities, tileData);
-        nextCell.possibilities = [tileIndex];
-        nextCell.entropy = 0;
-        currentTries++;
-
-        layer.putTileAt(tileIndex, nextCell.x, nextCell.y);
-        nextCell.collapsed = true;
-
-        let success = this.propagate(waveTable, tileData, nextCell, layer);
-        if (!success) {
-            // On contradiction, reset and try again with new seed
-            this.initializeWaveTable(waveTable, defaultTable, width, height);
-            this.initializeBiomeSeeds(waveTable, biomeMap, tileData);
-            
-            const newX = Math.floor(Math.random() * width);
-            const newY = Math.floor(Math.random() * height);
-            const newBiome = biomeMap[newY][newX];
-            const newTile = TERRAIN_TYPES[newBiome][
-                Math.floor(Math.random() * TERRAIN_TYPES[newBiome].length)
-            ];
-            
-            const newStart = waveTable[newY][newX];
-            newStart.possibilities = [newTile];
-            newStart.entropy = 0;
-            newStart.collapsed = true;
-
-            layer.putTileAt(newTile, newX, newY);
-            this.propagate(waveTable, tileData, newStart, layer);
-
-            current = newStart;
-            currentTries++;
-            continue;
-        }
-
-        current = nextCell;
-    }
-    
-    this.checkAllCollapsed(waveTable, width, height);
 }
