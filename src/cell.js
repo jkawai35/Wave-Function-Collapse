48476df326a5c3fd72a909c286827f90ae6db36e
JustinThai-7 <justinthai.x@gmail.com>
2025-06-05 08:23:22 -0700

    new entropy implementation

--- src/cell.js
+++ src/cell.js
@@ -1,12 +1,12 @@
 
-class Cell extends Phaser.Scene {
-    constructor(entropy, possibilities, x, y) {
-        super('Cell')
-        this.entropy = entropy;
-        this.possibilities = possibilities
-        this.x = x
-        this.y = y
-        this.neighbors = []
-        this.collapsed = false
-    }
-}
+// class Cell extends Phaser.Scene {
+//    constructor(entropy, possibilities, x, y) {
+//        super('Cell')
+//        this.entropy = entropy;
+//        this.possibilities = possibilities
+//        this.x = x
+//        this.y = y
+//        this.neighbors = []
+//        this.collapsed = false
+//    }
+//}
